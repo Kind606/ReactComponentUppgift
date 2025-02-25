55e3d7c4f833cdc6765707bde73944eef51a1a21
Kind606 <kevin.hellgren@hotmail.com>
2025-02-25 20:51:56 +0100

    Added more styling to app and setup for toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { Outlet } from "react-router";
+import styled, { ThemeProvider } from "styled-components";
+import { darkTheme, lightTheme } from "./styleComponents/theme";
+
+const Header = styled.header`
+  background: ${({ theme }) =>
+    theme.secondBackground}; // Background changes based on theme
+  display: flex;
+  justify-content: space-between;
+`;
 
 export default function App() {
+  const [theme, setTheme] = useState("light");
+  const themeToggler = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
+
   return (
-    <div>
-      <header>
-        <h1>Hello World</h1>
-      </header>
-      <aside>
-        <p>SideBar</p>
-      </aside>
-      <main>
-        <Outlet />
-      </main>
-    </div>
+    <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+      <>
+        <div>
+          <Header>
+            <h1>Hello World</h1>
+          </Header>
+          <aside>
+            <p>SideBar</p>
+          </aside>
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </>
+    </ThemeProvider>
   );
 }
---
+++ src/styleComponents/theme.ts
@@ -0,0 +1,17 @@
+export const lightTheme = {
+  body: "#FFF",
+  text: "#363537",
+  secondText: "#FFF",
+  toggleBorder: "#FFF",
+  background: "#999",
+  secondBackground: "#44b32b",
+};
+
+export const darkTheme = {
+  body: "#363537",
+  text: "#FAFAFA",
+  secondText: "#FFF",
+  toggleBorder: "#6B8096",
+  background: "#363537",
+  secondBackground: "#235418",
+};
